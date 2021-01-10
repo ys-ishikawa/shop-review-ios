654d709dd4727dd8683fe65b0d0e3b9fbed8a5f3
yosuke-ishikawa <yosuke1028.ishikawa@gmail.com>
2021-01-10 10:21:12 +0900

    プロジェクトの作成

---
+++ ShopReview/ShopReview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShopReview
+//
+//  Created by Yosuke Ishikawa on 2021/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShopReview/ShopReview/ShopReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopReviewApp.swift
+//  ShopReview
+//
+//  Created by Yosuke Ishikawa on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct ShopReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
